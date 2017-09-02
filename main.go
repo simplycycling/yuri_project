4dc41aebe068c824a691515da5d7b2b7651461b9
Roger Sherman <rsherman@linux.com>
2017-09-02 14:00:54 +1000

    Not quite there yet, but I've figured out how to access the request, as a string or url. Next up - get the request to accept a query.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/url"
 )
 
 func main() {
@@ -12,22 +11,20 @@ func main() {
 }
 
 func endpoint(w http.ResponseWriter, r *http.Request) {
-	s := "localhost:3000/hello?name=Roger"
-
-	u, err := url.Parse(s)
-	if err != nil {
-		panic(err)
-	}
-
-	m, _ := url.ParseQuery(u.RawQuery)
-	nameString := "Hello" + " " + m["name"][0] + "!"
-	fmt.Fprint(w, nameString)
-
+	urlString := r.URL.String()
+	//u, err := url.Parse(urlString)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//m, _ := url.ParseQuery(u.RawQuery)
+	//nameString := "Hello" + " " + m["name"][0] + "!"
+	nameString := "Hello" + " " + urlString + "!"
+	fmt.Fprintln(w, nameString)
 }
 
 
 // First assignment by Yuri, a simple app that will return "Hello World"
 // when called by cURL.
-// Attempting to rewrite so that a url query hello?name=Roger returns
-// Hello Roger. Not quite there yet, but I think I have a piece of the
-// puzzle - the url parsing. Now I have to figure out how to parse the actual request.
+// Second assignment: rewrite so that a url query hello?name=Roger returns
+// Hello Roger.
